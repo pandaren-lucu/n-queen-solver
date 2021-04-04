fe805128e9deb1f8e0b0d6a9a245c9526dbec3b3
Bunga Amalia Kurniawati <bungasokia@gmail.com>
2021-04-04 18:20:18 +0700

    [ADD] implement minisat on GUI

--- main.py
+++ main.py
@@ -17,7 +17,16 @@ while event != sg.WIN_CLOSED and event != 'Cancel':
             window['-ERROR-'].update('The board size must be between 8-16')
         else:
             window.close()
-            board = [[sg.Button(f'[{row}, {col}]', enable_events=True) for col in range(board_size)] for row in range(board_size)]
+            board_dict = {}
+            board = []
+            idx = 1
+            for row in range(board_size):
+                temp_row = []
+                for col in range(board_size):
+                    temp_row.append(sg.Button(f'[{row}, {col}]', enable_events=True, key=idx))
+                    board_dict[idx] = f'[{row}, {col}]'
+                    idx += 1
+                board.append(temp_row)
             layout= [
                 [sg.Frame('Chess Board', board)],
                 [sg.Text(size=(60,1), key='-ERROR-')],
@@ -30,17 +39,39 @@ while event != sg.WIN_CLOSED and event != 'Cancel':
                 window[event].update(disabled=True)
                 event, values = window.read()
             window.close()
-            board = [[sg.Text(f'[{row}, {col}]', key=f'[{row}, {col}]') for col in range(board_size)] for row in range(board_size)]
+            board = []
+            idx = 1
+            for row in range(board_size):
+                temp_row = []
+                for col in range(board_size):
+                    temp_row.append(sg.Text(f'[{row}, {col}]', key=idx))
+                    idx += 1
+                board.append(temp_row)
             layout= [
                 [sg.Frame('Chess Board', board)],
                 [sg.Text(size=(60,1), key='-ERROR-')],
                 [sg.Button('Next'), sg.Button('Cancel')]]
             window = sg.Window('N-Queen Solver by Pandaren Lucu', layout)
             event, values = window.read()
+            clauses = sat.generate_n_queen_clauses(board_size)
+            for queen in start_queen:
+                clauses.append([queen])
+            print('clauses', clauses)
+            print('start_queen', start_queen)
             while event != 'Cancel':
-                for queen in start_queen:
-                    window[queen].update('QQQQ')
-                print(start_queen)
+                solution = sat.solve_n_queen(board_size, clauses)
+                if solution[1] == None:
+                    sg.popup('There is no more solution!')
+                    if event == 'OK':
+                        window.close()
+                for queen in solution[2]:
+                    if queen > 0:
+                        window[queen].update('QQQQ')
+                    else:
+                        window[-queen].update(board_dict[-queen])
+                clauses.append([-queen for queen in solution[2]])
+                print('solution', solution)
+                print(event, values)
                 event, values = window.read()
     except:
         window['-ERROR-'].update('The board size must be an integer')
