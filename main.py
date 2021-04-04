6da0840f9425b1526ee9f3dcf282d40a10bd512f
Bunga Amalia Kurniawati <bungasokia@gmail.com>
2021-04-04 10:33:16 +0700

    [ADD] implement basic chess board

--- main.py
+++ main.py
@@ -4,25 +4,46 @@ import PySimpleGUI as sg
 sg.theme('TanBlue')
 
 layout = [  [sg.Text('Please input the board size (8-16)')],
-            [sg.Text('Board size:'), sg.InputText(key='-BOARD-')],
+            [sg.Text('Board size:'), sg.InputText(key='-SIZE-')],
             [sg.Text(size=(60,1), key='-ERROR-')],
-            [sg.Button('Ok'), sg.Button('Cancel')] ]
+            [sg.Button('Start'), sg.Button('Cancel')] ]
 
 window = sg.Window('N-Queen Solver by Pandaren Lucu', layout)
-
-while True:
+event, values = window.read()
+while event != sg.WIN_CLOSED and event != 'Cancel':
+    try:
+        board_size = int(values['-SIZE-'])
+        if board_size < 8 or board_size > 16:
+            window['-ERROR-'].update('The board size must be between 8-16')
+        else:
+            window.close()
+            board = [[sg.Button(f'[{row}, {col}]', enable_events=True) for col in range(board_size)] for row in range(board_size)]
+            layout= [
+                [sg.Frame('Chess Board', board)],
+                [sg.Text(size=(60,1), key='-ERROR-')],
+                [sg.Button('Solve'), sg.Button('Cancel')]]
+            window = sg.Window('N-Queen Solver by Pandaren Lucu', layout)
+            event, values = window.read()
+            start_queen = []
+            while event != 'Solve':
+                start_queen.append(event)
+                window[event].update(disabled=True)
+                event, values = window.read()
+            window.close()
+            board = [[sg.Text(f'[{row}, {col}]', key=f'[{row}, {col}]') for col in range(board_size)] for row in range(board_size)]
+            layout= [
+                [sg.Frame('Chess Board', board)],
+                [sg.Text(size=(60,1), key='-ERROR-')],
+                [sg.Button('Next'), sg.Button('Cancel')]]
+            window = sg.Window('N-Queen Solver by Pandaren Lucu', layout)
+            event, values = window.read()
+            while event != 'Cancel':
+                for queen in start_queen:
+                    window[queen].update('QQQQ')
+                print(start_queen)
+                event, values = window.read()
+    except:
+        window['-ERROR-'].update('The board size must be an integer')
+    print(values)
     event, values = window.read()
-    if event == 'Ok':
-        try:
-            board_size = int(values['-BOARD-'])
-            if board_size < 8 or board_size > 16:
-                window['-ERROR-'].update('The board size must be between 8-16')
-            else:
-                window['-ERROR-'].update('YEY')
-        except:
-            window['-ERROR-'].update('The board size must be an integer')
-    if event == sg.WIN_CLOSED or event == 'Cancel':
-        break
-    print('You entered ', values['-BOARD-'])
-
 window.close()
