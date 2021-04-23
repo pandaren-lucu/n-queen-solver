d383ed03d53b9d20f1a19829588aea09c6d3a70e
Bunga Amalia Kurniawati <bungasokia@gmail.com>
2021-04-23 17:05:36 +0700

    [ADD] documentation, readme, and gitignore

---
+++ README.md
@@ -0,0 +1,14 @@
+# N-Queen Solver by Pandaren Lucu
+
+Solve [N-Queen Problems](https://en.wikipedia.org/wiki/Eight_queens_puzzle) with [MiniSat](http://minisat.se/) and [PySimpleGUI](https://pysimplegui.readthedocs.io/en/latest/).
+
+Created by:
+- Pande Ketut Cahya Nugraha
+- Naufal Pratama Tanansyah
+- Bunga Amalia Kurniawati
+
+To start the program:
+1. Install MiniSat. For windows user, please follow [this](http://web.cecs.pdx.edu/~hook/logicw11/Assignments/MinisatOnWindows.html). For Ubuntu user, please follow [this](https://www.howtoinstall.me/ubuntu/18-04/minisat/). Make sure that you have add the MiniSat to the environment variables.
+2. Install Python 3. If you haven't install it, please follow [this](https://realpython.com/installing-python/).
+3. Install the requirements. Run `pip install -r requirements.txt`.
+4. Run the program with `python main.py`.
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sat
 import PySimpleGUI as sg
 
+# Create layout for board size form
 sg.theme('Black')
 
 layout = [  [sg.Text('Please input the board size (8-15)')],
@@ -11,10 +12,12 @@ layout = [  [sg.Text('Please input the board size (8-15)')],
 window = sg.Window('N-Queen Solver by Pandaren Lucu', layout)
 event, values = window.read()
 
+# Check inputted board size and save it
 while event != sg.WIN_CLOSED:
     try:
         board_size = int(values['-SIZE-'])
 
+        # Handle board size out of range error
         if board_size < 8 or board_size > 15:
             window['-ERROR-'].update('The board size must be between 8-15')           
         else:
@@ -23,6 +26,7 @@ while event != sg.WIN_CLOSED:
             board = []
             idx = 1
 
+            # Generate initial chess board with button
             for row in range(board_size):
                 temp_row = []
 
@@ -35,8 +39,11 @@ while event != sg.WIN_CLOSED:
                     idx += 1
                 board.append(temp_row)
 
+            # Create layout for initial chess board with button
             layout= [
                 [sg.Frame('Chess Board', board)],
+                [sg.Text('You can optionally put starter Queen(s) wherever you want before solving the problem.')],
+                [sg.Text('The MiniSat will gives you solution, if there is any, that always contain the starter Queen(s).')],
                 [sg.Text(size=(40,1), key='-ERROR-')],
                 [sg.Button('Solve', size=(10,2))]]
 
@@ -44,6 +51,7 @@ while event != sg.WIN_CLOSED:
             event, values = window.read()
             start_queen = []
 
+            # Save all starting Queens
             while event != 'Solve':
                 start_queen.append(event)
                 window[event].update('Queen', button_color=('white', 'yellow'), disabled=True)
@@ -53,6 +61,7 @@ while event != sg.WIN_CLOSED:
             board = []
             idx = 1
 
+            # Generate solution chess board with disabled button
             for row in range(board_size):
                 temp_row = []
                 for col in range(board_size):
@@ -63,6 +72,7 @@ while event != sg.WIN_CLOSED:
                     idx += 1
                 board.append(temp_row)
 
+            # Create layout for solution chess board with disabled button
             layout= [
                 [sg.Frame('Chess Board', board)],
                 [sg.Text(size=(40,1), key='-ERROR-')],
@@ -71,18 +81,24 @@ while event != sg.WIN_CLOSED:
             window = sg.Window('N-Queen Solver by Pandaren Lucu', layout)
             event, values = window.read()
             window['-NEXT-'].update('Next solution')
+
+            # Generate clauses for MiniSat
             clauses = sat.generate_n_queen_clauses(board_size)
 
+            # Insert starting Queens to clauses
             for queen in start_queen:
                 clauses.append([queen])
 
             while event != sg.WIN_CLOSED:
+                # Generate solution by sending the clauses to MiniSat
                 solution = sat.solve_n_queen(board_size, clauses)
 
+                # Close program is there is no more solution available
                 if solution[1] == None:
                     sg.popup('There is no more solution!')
                     window.close()
 
+                # Display N-Queen problem solution
                 for queen in solution[2]:
                     if queen > 0:
                         window[queen].update('Queen', button_color=('white', 'yellow'))
@@ -91,12 +107,16 @@ while event != sg.WIN_CLOSED:
                     else:
                         window[-queen].update(board_dict[-queen][0], button_color=('black', 'white'))
                         
+                # Insert the negation of viewed solution to the clauses
                 clauses.append([-queen for queen in solution[2]])
                 event, values = window.read()
 
+    # Handle board size not an integer error
     except:
         window['-ERROR-'].update('The board size must be an integer')
 
+    # Read event and values
     event, values = window.read()
 
+# Close the program
 window.close()
